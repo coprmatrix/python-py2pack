eea29862a43face760631a3e22c5ca6a71c1ece1
lskfjwer <uiguruigurovich@gmail.com>
2025-02-24 16:25:30 +0300

    update

--- .copr/main.sh
+++
@@ -1,5 +0,0 @@
-dnf install -y tox git python3-build python3-hatchling
-git clone https://github.com/huakim/py2pack.git
-. ./generate_sources.sh
-rpmbuild "-D_srcrpmdir ${outdir}" "-D_sourcedir ${outdir}" --bs "${outdir}/python-py2pack.spec"
-
